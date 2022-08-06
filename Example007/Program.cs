93c17bc77c3d49160b61caa3170c74073357b27a
NadyaN <nadyanaidjonova@gmail.com>
2022-08-06 15:35:40 +0300

    является ли врое число квадратным корнем первого

---
+++ Example007/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите числа ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+if (Math.Sqrt(numberA) == numberB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
