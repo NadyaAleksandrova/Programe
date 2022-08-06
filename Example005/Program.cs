0a5196d5fa3cf53817e4f8c460125c858230b6cd
NadyaN <nadyanaidjonova@gmail.com>
2022-08-06 15:00:31 +0300

    Приветствие любимчику

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Я так тебя ждала, Маша!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
