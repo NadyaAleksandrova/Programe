00802bf5eaed79646fb637d9aa0a4ea764b5b6ba
NadyaN <nadyanaidjonova@gmail.com>
2022-08-08 17:11:01 +0300

    Максимальное из трех чисел

---
+++ Homework0508Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите значение: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB && numberA > numberC)
+{Console.WriteLine("Максимальное число " + numberA);}
+else if (numberB > numberC)
+{Console.WriteLine("Максимальное число " + numberB);}
+else
+{Console.WriteLine("Максимальное число " + numberC);}
