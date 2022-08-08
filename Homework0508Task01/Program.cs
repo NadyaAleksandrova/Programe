2ea16d1c348c32a9d142d06f0cc702b959cc3d3d
NadyaN <nadyanaidjonova@gmail.com>
2022-08-08 16:10:48 +0300

    Задача на поиск большего и меньшего числа

---
+++ Homework0508Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите числа ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine ("Первое число больше, второе число меньше");
+}
+else
+{
+    Console.WriteLine ("Второе число больше, первое число меньше");
+}
