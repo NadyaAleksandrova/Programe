e008813e5c598178e0d7db74d563039485879de6
NadyaN <nadyanaidjonova@gmail.com>
2022-08-10 14:01:26 +0300

    Выходной или нет

---
+++ Homework0908Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Введен не верный формат дня недели");
+}
+if (number >= 6 && number <= 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
