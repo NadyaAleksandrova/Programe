7f43870987bc962ecc19fe79acac34d358b8fe01
NadyaN <nadyanaidjonova@gmail.com>
2022-08-10 13:49:45 +0300

    Вывод третьей цифры/сообщение что её нет

---
+++ Homework0908Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+string str = num.ToString();
+if (str.Length > 2)
+{
+    Console.WriteLine(str[2]);
+}
+else
+{
+  Console.WriteLine("нет третьего числа");  
+}
