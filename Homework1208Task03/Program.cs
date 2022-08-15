65e611a3a4bbc7070dfa8887a5ae9c057a618ad3
NadyaN <nadyanaidjonova@gmail.com>
2022-08-15 14:14:42 +0300

    таблица кубов

---
+++ Homework1208Task03/Program.cs
@@ -0,0 +1,6 @@
+﻿// Принимает на вход число N и выдает таблицу кубов от 1 до N
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.WriteLine("{0}^3 = {1}", i, i * i * i);
+
