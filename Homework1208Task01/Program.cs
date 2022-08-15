5585651dc26e3d77ee9fb11c7e90c8f8b917f0e2
NadyaN <nadyanaidjonova@gmail.com>
2022-08-15 11:41:24 +0300

    Полиндром

---
+++ Homework1208Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+string str = num.ToString();
+if (str.Length < 4 || str.Length > 4)
+{
+    Console.WriteLine("Введен не верный формат числа");
+    return;
+}
+if (str[0] == str[4] && str[1] == str[3])
+{
+    Console.WriteLine("полиндром");
+}
+
+else
+{
+    Console.WriteLine("не является полиндромом");
+}
