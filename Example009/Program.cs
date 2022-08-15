fe00b4fd889279e72ef9092e2a9592e54e96511a
NadyaN <nadyanaidjonova@gmail.com>
2022-08-15 10:59:51 +0300

    нахождение четверти

---
+++ Example009/Program.cs
@@ -0,0 +1,17 @@
+﻿void quatr(int x, int y)
+{
+    if (x == 0 || y == 0)
+    Console.WriteLine ("Ошибка");
+    if (x > 0 && y > 0)
+    Console.WriteLine ("1 четверть");
+    if (x < 0 && y > 0)
+    Console.WriteLine ("2 четверть");
+    if (x < 0 && y < 0)
+    Console.WriteLine ("3 четверть");
+    if (x > 0 && y < 0)
+    Console.WriteLine ("4 четверть");
+}
+
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+quatr (x, y);
