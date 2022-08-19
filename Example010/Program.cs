a4f8c6364456fdcb55c3e4ac66a742df81f81fb3
NadyaN <nadyanaidjonova@gmail.com>
2022-08-19 13:48:11 +0300

    сумма и произведение всех чисел, входящих в состав числа

---
+++ Example010/Program.cs
@@ -0,0 +1,22 @@
+﻿static int getnumber()
+{
+    int number = 0;
+    do
+    {
+Console.WriteLine("Введите число А");
+number = int.TryParse(Console.ReadLine(), out number) ? number : 0;
+    } while (number <= 0);
+    return number;
+}
+
+int n = getnumber();
+int sum=0;
+int mul=1;
+
+for (int i= 1; i <= n; i++)
+{
+    sum+=i;
+    mul*=i;
+}
+Console.WriteLine("сумма чисел:   " + sum);
+Console.WriteLine("произведение чисел:   " + mul);
