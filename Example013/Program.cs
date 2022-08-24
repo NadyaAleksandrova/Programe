0ce230547c670289f0b05249abb55006b5ec3394
NadyaN <nadyanaidjonova@gmail.com>
2022-08-24 15:38:28 +0300

    Разворот массива

---
+++ Example013/Program.cs
@@ -0,0 +1,16 @@
+﻿void FillArray(int[] array)
+{
+    int b = array.Length - 1;
+    int[] array1 = new int[8];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i]= new Random().Next(0,100);
+        array1[b] = array[i];
+        b--;
+    }
+    Console.WriteLine(string.Join(", ", array));
+    Console.WriteLine(string.Join(", ", array1));
+}
+
+int [] array = new int[8];
+FillArray(array);
