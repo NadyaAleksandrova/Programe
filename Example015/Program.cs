b587ee30a4910c23c3f4c3279d353b9e1dfbe601
NadyaN <nadyanaidjonova@gmail.com>
2022-08-24 15:56:12 +0300

    Среднееарифметическое положительных элементов массива

---
+++ Example015/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+ for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(-10,11);
+    }
+Console.WriteLine(string.Join(", ", array));
+double sum = 0;
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        sum = sum + array[i];
+        count++;
+    }
+}
+Console.WriteLine($"Среднее арифметическoе положительных элементов масива равно {sum/count}");
