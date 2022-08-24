04292d5349c8f2333288313579b64b4c81ac4178
NadyaN <nadyanaidjonova@gmail.com>
2022-08-24 16:13:32 +0300

    Переворот массива вариант 2

---
+++ Example016/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] FillArray(int capacity)
+{
+    int[] array = new int[capacity];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+void arrayReverse(int[] array)
+{
+    int size = array.Length;
+    for(int i = 0; i < size / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[size - i - 1];
+        array[size - i - 1] = temp;
+    }
+}
+
+int[] arr = FillArray(10);
+Console.WriteLine();
+arrayReverse(arr);
+
+Console.WriteLine(string.Join(", ", arr));
