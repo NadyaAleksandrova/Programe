f42d238a24e17226f02e7ce72bea0af91e6e4824
NadyaN <nadyanaidjonova@gmail.com>
2022-08-24 15:46:13 +0300

    Среднееарифметическое массива

---
+++ Example014/Program.cs
@@ -0,0 +1,16 @@
+﻿double FillArray(int[] array)
+{
+    double sum = 0;
+    double result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(-10,11);
+        sum = sum + array[i];
+    }
+    Console.WriteLine(string.Join(", ", array));
+    result = Math.Round(sum/(array.Length),2);
+    return result;
+}
+
+int [] array = new int[8];
+Console.WriteLine (FillArray(array));
