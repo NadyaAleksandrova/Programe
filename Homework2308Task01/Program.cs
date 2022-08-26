ff82a8674b9350fc33055e25629fc5c525426472
NadyaN <nadyanaidjonova@gmail.com>
2022-08-26 13:02:53 +0300

    Счетчик чисел боле 0

---
+++ Homework2308Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
