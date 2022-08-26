8f85ad9a36132da46877743d2178fb0a8d2e6a67
NadyaN <nadyanaidjonova@gmail.com>
2022-08-26 14:44:54 +0300

    Сумма элементов главной диагонали

---
+++ Homework2308Task03/Program.cs
@@ -0,0 +1,44 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+int Sum(int[,] matr)
+{
+    int Sumelement = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                Sumelement = Sumelement + matr[i, j];
+            }
+        }
+    }
+    return Sumelement;
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+int Sumelement = Sum(matrix);
+Console.WriteLine($"Сумма элеметов по диагонали: {Sumelement}");
