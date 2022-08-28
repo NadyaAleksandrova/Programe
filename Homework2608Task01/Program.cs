5eaf6a417567d33c02fdb51da2286116b263eb35
NadyaN <nadyanaidjonova@gmail.com>
2022-08-28 11:04:10 +0300

    массив заполненый вещественными числами

---
+++ Homework2608Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+
+    }
+    return;
+}
+void WriteArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write(alignNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] array = new double[m, n];
+FillArray(array);
+WriteArray(array);
