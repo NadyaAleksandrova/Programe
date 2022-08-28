b115ca233bbf61559bb9568bc2ec59a21b5e9962
NadyaN <nadyanaidjonova@gmail.com>
2022-08-28 10:31:52 +0300

    Квадрат элементов с четными индексами

---
+++ Example017/Program.cs
@@ -0,0 +1,52 @@
+﻿// найти элементы с четными индексами и заменить их на квадраты
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+
+    }
+    return;
+}
+
+void ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return;
+}
+
+Console.WriteLine("Введите количество строк и столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+ChangeArray(array);
+PrintArray(array);
