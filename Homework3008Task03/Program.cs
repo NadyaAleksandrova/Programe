3084606bd3e0ac2e78950f598cd65b60e412420b
NadyaN <nadyanaidjonova@gmail.com>
2022-08-31 13:03:37 +0300

    Произведение двух матриц

---
+++ Homework3008Task03/Program.cs
@@ -0,0 +1,99 @@
+﻿Console.WriteLine("Введите количество строк и столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+Console.WriteLine("Введите количество строк и столбцов: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int l = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array2 = new int[k, l];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+FillArray2(array2);
+PrintArray2(array2);
+Console.WriteLine("Произведение двух матриц: ");
+MultArray(array, array2);
+PrintMultArray(array, array2);
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+
+    }
+    return;
+}
+
+void FillArray2(int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[i, j] = new Random().Next(0, 10);
+        }
+
+    }
+    return;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return;
+}
+void PrintArray2(int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return;
+}
+
+
+
+int[,] MultArray(int[,] array, int[,] array2)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[i, j] = array[i, j] * array2[i, j];
+        }
+    }
+    return result;
+}
+
+void PrintMultArray(int[,] array, int[,] array2)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return;
+}
+
