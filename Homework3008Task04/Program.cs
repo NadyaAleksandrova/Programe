9f7dcf22fb83272b014920d8dd5cfead0087ba16
NadyaN <nadyanaidjonova@gmail.com>
2022-08-31 12:32:27 +0300

    Трехмерный массив двузначных чисел с индексами

---
+++ Homework3008Task04/Program.cs
@@ -0,0 +1,36 @@
+﻿void FillArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+Console.WriteLine("Введите размер массива ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int p = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,,] array = new int[m, n, p];
+FillArray(array);
+PrintArray(array);
