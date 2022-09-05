c721da3daff69bd21283b562ffc6c367726af835
NadyaN <nadyanaidjonova@gmail.com>
2022-09-05 09:29:51 +0300

    change to right

--- Homework3008Task03/Program.cs
+++ Homework3008Task03/Program.cs
@@ -1,47 +1,30 @@
-﻿Console.WriteLine("Введите количество строк и столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-int[,] array = new int[m, n];
-Console.WriteLine("Введите количество строк и столбцов: ");
-int k = Convert.ToInt32(Console.ReadLine());
-int l = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-int[,] array2 = new int[k, l];
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-FillArray2(array2);
-PrintArray2(array2);
-Console.WriteLine("Произведение двух матриц: ");
-MultArray(array, array2);
-PrintMultArray(array, array2);
-
-
-void FillArray(int[,] array)
+﻿int[,] MatrixMultiplic(int[,] arrayOne, int[,] arrayTwo)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(0, 10);
-        }
+    int[,] arrayResult = new int[arrayOne.GetLength(0), arrayTwo.GetLength(1)];
 
+    for(int i = 0; i < arrayResult.GetLength(0); i++)
+    {
+        for(int j = 0; j < arrayResult.GetLength(1); j++)
+        {
+            for(int k = 0; k < arrayOne.GetLength(1); k++)
+            {arrayResult[i,j] += arrayOne[i,k] * arrayTwo[k, j];}
+        }
     }
-    return;
+
+    return arrayResult;
 }
 
-void FillArray2(int[,] array2)
+int[,] FillArray(int m = 4, int n = 4)
 {
-    for (int i = 0; i < array2.GetLength(0); i++)
+    int[,] array = new int [m, n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < array2.GetLength(1); j++)
+        for(int j = 0; j < n; j++)
         {
-            array2[i, j] = new Random().Next(0, 10);
+            array[i, j] = new Random().Next(1, 11);
         }
-
     }
-    return;
+    return array;
 }
 
 void PrintArray(int[,] array)
@@ -49,51 +32,18 @@ void PrintArray(int[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
+        {Console.Write("{0,4}", array[i, j]);}
         Console.WriteLine();
     }
-    return;
-}
-void PrintArray2(int[,] array2)
-{
-    for (int i = 0; i < array2.GetLength(0); i++)
-    {
-        for (int j = 0; j < array2.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    return;
 }
 
+int[,] newArray1 = FillArray();
+PrintArray(newArray1);
+Console.WriteLine();
 
+int[,] newArray2 = FillArray();
+PrintArray(newArray2);
+Console.WriteLine();
 
-int[,] MultArray(int[,] array, int[,] array2)
-{
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            result[i, j] = array[i, j] * array2[i, j];
-        }
-    }
-    return result;
-}
-
-void PrintMultArray(int[,] array, int[,] array2)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    return;
-}
-
+int[,] newArrayResult = MatrixMultiplic(newArray1, newArray2);
+PrintArray(newArrayResult);
