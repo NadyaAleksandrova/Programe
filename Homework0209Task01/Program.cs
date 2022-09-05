38a5c1ca536aa0bcbdc2ecb2e236be5736513119
NadyaN <nadyanaidjonova@gmail.com>
2022-09-05 10:39:01 +0300

    все натуральные числа в промежутке от M до N

---
+++ Homework0209Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿int M = AskNumber("M");
+int N = AskNumber("N");
+Console.WriteLine($"{PrintNumber(M, N)}");
+
+string PrintNumber(int M, int N)
+{
+    if (M == N) return N.ToString();
+    else return "" + M.ToString() + ", " + PrintNumber(++M, N);
+
+}
+
+int AskNumber(string name)
+{
+    Console.Write($"Введите число {name}: ");
+    return int.Parse(Console.ReadLine());
+}
