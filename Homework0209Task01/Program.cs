fbce8640e5a0789b42dc8dc9e52117a23d265aee
NadyaN <nadyanaidjonova@gmail.com>
2022-09-05 10:59:51 +0300

    change to right

--- Homework0209Task01/Program.cs
+++ Homework0209Task01/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine($"{PrintNumber(M, N)}");
 
 string PrintNumber(int M, int N)
 {
+    if (M < N || M <= 0 || N <= 0) return "";
     if (M == N) return N.ToString();
     else return "" + M.ToString() + ", " + PrintNumber(++M, N);
 
