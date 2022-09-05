e4af77cccd4dde6cfcd182a994249e9262819a66
NadyaN <nadyanaidjonova@gmail.com>
2022-09-05 10:46:11 +0300

    сумма натуральных элементов от M дo N

---
+++ Homework0209Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+
+int M = AskNumber("M");
+int N = AskNumber("N");
+
+void NumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    NumberSum(M, N, sum);
+}
+
+int AskNumber(string name)
+{
+    Console.Write($"Введите число {name}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+NumberSum(M, N, 0);
